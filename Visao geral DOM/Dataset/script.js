46b506ea309cceb1b6a436c523f31ffa45e0b1b7
Gerson Aparecido Lima <gerson.lima@yduqs.com.br>
2023-12-22 12:13:40 -0300

    Dataset

---
+++ Visao geral DOM/Dataset/script.js
@@ -0,0 +1,11 @@
+
+//querySelector vc colocar o seletor css 
+//const card1 = document.querySelector('.card')
+//console.log(card1);
+//console.log(card1.innerHTML);
+
+
+//Selecionando o atributo data set
+const card1 = document.querySelector('[data-product-id="32"]');
+//console.log(card1);
+console.log(card1.innerHTML);
